ed7deb45a5503dbe49c1bf8fef3f96f99773f29e
bagasrizq <bagas.rizqsokya@gmail.com>
2023-09-30 14:00:22 +0700

    commit 2

---
+++ Week 7/homework/soal1/modul.js
@@ -0,0 +1,6 @@
+exports.persegi = function (sisi) {
+    return sisi * sisi;
+}
+exports.persegiPanjang = function (panjang, lebar) {
+    return panjang * lebar;
+}
---
+++ Week 7/homework/soal1/soal1.js
@@ -0,0 +1,4 @@
+const modul = require ('./modul.js');
+
+console.log('Rumus persegi  sisi * sisi = '+modul.persegi(5));
+console.log('Rumus persegi panjang * lebar = '+modul.persegiPanjang(5, 4));
